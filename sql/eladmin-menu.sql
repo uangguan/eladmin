7b0466dab97743cea0fe1146b11ccf2aa10c0c9d
uangguan <weiguanhuang@gmail.com>
2020-04-11 22:30:51 +0800

    增加商品管理，商品分类管理，添加菜单

--- sql/eladmin-menu.sql
+++ sql/eladmin-menu.sql
@@ -1,7 +1,7 @@
-INSERT INTO `menu` VALUES (117, b'0', '商品新增', '', 23, 2, '', '', b'0', b'0', '', '2020-03-29 10:59:46', ':product:add', 2);
-INSERT INTO `menu` VALUES (118, b'0', '商品编辑', '', 23, 3, '', '', b'0', b'0', '', '2020-03-29 11:00:08', 'product:edit', 2);
-INSERT INTO `menu` VALUES (119, b'0', '商品删除', '', 23, 4, '', '', b'0', b'0', '', '2020-03-29 11:00:23', 'product:del', 2);
+INSERT INTO `menu` VALUES (117, b'0', '商品新增', '', 23, 1, '', '', b'0', b'0', '', '2020-03-29 10:59:46', 'product:add', 2);
+INSERT INTO `menu` VALUES (118, b'0', '商品编辑', '', 23, 2, '', '', b'0', b'0', '', '2020-03-29 11:00:08', 'product:edit', 2);
+INSERT INTO `menu` VALUES (119, b'0', '商品删除', '', 23, 3, '', '', b'0', b'0', '', '2020-03-29 11:00:23', 'product:del', 2);
 
-INSERT INTO `menu` VALUES (120, b'0', '商品分类新增', '', 22, 2, '', '', b'0', b'0', '', '2020-03-29 10:59:46', 'productCatagory:add', 2);
-INSERT INTO `menu` VALUES (121, b'0', '商品分类编辑', '', 22, 3, '', '', b'0', b'0', '', '2020-03-29 11:00:08', 'productCatagory:edit', 2);
-INSERT INTO `menu` VALUES (122, b'0', '商品分类删除', '', 22, 4, '', '', b'0', b'0', '', '2020-03-29 11:00:23', 'productCatagory:del', 2);
+INSERT INTO `menu` VALUES (120, b'0', '商品分类新增', '', 22, 1, '', '', b'0', b'0', '', '2020-03-29 10:59:46', 'productCatagory:add', 2);
+INSERT INTO `menu` VALUES (121, b'0', '商品分类编辑', '', 22, 2, '', '', b'0', b'0', '', '2020-03-29 11:00:08', 'productCatagory:edit', 2);
+INSERT INTO `menu` VALUES (122, b'0', '商品分类删除', '', 22, 3, '', '', b'0', b'0', '', '2020-03-29 11:00:23', 'productCatagory:del', 2);
