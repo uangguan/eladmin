8f98732d009b3dd5be804d8b5a29abce3199af9d
uangguan <weiguanhuang@gmail.com>
2020-04-12 12:46:36 +0800

    增加商品管理，商品分类管理

--- sql/eladmin-product.sql
+++ sql/eladmin-product.sql
@@ -9,6 +9,6 @@ VALUES
 
 INSERT INTO `product` (`id`, `name`, `describe`, `specification`, `category_id`, `count`, `original_price`, `activity_price`, `merchant_id`, `enabled`, `create_time`)
 VALUES
-	(1, 'MacBook pro 16', 'macbook pro 16 inch', '16 inch ', 5, 14000, 15000, 10, 14, 1, '2020-03-29 11:00:08'),
-	(2, 'MacBook pro 15', 'macbook pro 15 inch', '15 inch ', 5, 13000, 14000, 10, 14, 1, '2020-03-29 11:00:08'),
-    (3, 'MacBook pro 13', 'macbook pro 13 inch', '13 inch ', 5, 12000, 13000, 10, 14, 1, '2020-03-29 11:00:08');
+	(1, 'MacBook pro 16', 'macbook pro 16 inch', '16 inch ', 5, 10, 16000, 15000, 14, 1, '2020-03-29 11:00:08'),
+	(2, 'MacBook pro 15', 'macbook pro 15 inch', '15 inch ', 5, 10, 15000, 14000, 14, 1, '2020-03-29 11:00:08'),
+    (3, 'MacBook pro 13', 'macbook pro 13 inch', '13 inch ', 5, 10, 14000, 13000, 14, 1, '2020-03-29 11:00:08');
